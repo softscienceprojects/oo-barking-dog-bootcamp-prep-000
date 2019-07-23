73551463041ac3d53c9799e11ff87d79cb7f62ef
erin johnson <erin.nicole.johnson@gmail.com>
2019-07-23 19:50:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,12 +4,16 @@ class Dog
          @name = name
        end
 
+       def name=(new_name)  #setter
+         @name = new_name
+       end
+
        def name  #getter
          @name
        end
 
-       def name=(new_name)  #setter
-         @name = new_name
+       def bark
+         puts "woof!"
        end
-     
+
 end
