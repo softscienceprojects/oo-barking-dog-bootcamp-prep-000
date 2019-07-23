b4b15d23fc2558e5cbf0a5962cfb7eca0116b06e
erin johnson <erin.nicole.johnson@gmail.com>
2019-07-23 19:50:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,8 @@
 # Your code goes here!
 class Dog
-  def initialize(name) #required
-         @name = name
-       end
+  # def initialize(name) #required
+  #        @name = name
+  #      end
 
        def name=(new_name)  #setter
          @name = new_name
